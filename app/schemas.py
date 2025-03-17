37c598533e5a28306f702a93508e321e766000eb
BileichukIvan <godail2205@gmail.com>
2025-03-17 17:20:04 +0200

    Added fields Marshmallow validation

--- app/schemas.py
+++ app/schemas.py
@@ -1,5 +1,6 @@
-from app.extensions import ma
+from app.extensions import ma, db
 from app.models import User
+from marshmallow import validate, ValidationError
 
 
 class UserSchema(ma.SQLAlchemyAutoSchema):
@@ -7,6 +8,29 @@ class UserSchema(ma.SQLAlchemyAutoSchema):
         model = User
         load_instance = True
 
+    name = ma.String(
+        required=True,
+        validate=[
+            validate.Length(
+                min=1, max=255, error="Name must be between 1 and 255 characters long."
+            )
+        ],
+    )
+
+    email = ma.String(
+        required=True,
+        validate=[
+            validate.Length(
+                min=1, max=255, error="Email must be between 1 and 255 characters long."
+            ),
+            validate.Email(error="Invalid email format."),
+        ],
+    )
+
+    @staticmethod
+    def validate_email(email):
+        if db.session.query(User).filter(User.email == email).first():
+            raise ValidationError("Email already exists.")
 
 user_schema = UserSchema()
 users_schema = UserSchema(many=True)
